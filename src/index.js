1cac426f2c6d29b2b247b889367775758808b9f8
Grace Alilu <graceegbi1@gmail.com>
2025-04-05 20:23:08 +0100

    Added JavaScript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("Welcome");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "La tombe dit à la rose",
+    autoStart: true,
+    delay: 5,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator");
+poemFormElement.addEventListener("submit", generatePoem);
