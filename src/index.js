4b1f3d889f49f10a11bbe2c093b6b9635ae051cf
Grace Alilu <graceegbi1@gmail.com>
2025-04-06 18:14:02 +0100

    Added API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
 function generatePoem(event) {
   event.preventDefault();
 
-  new Typewriter("#poem", {
-    strings: "The AI Poem Generator",
-    autoStart: true,
-    delay: 2,
-    cursor: "",
-  });
+  let instructionsInput = document.querySelector(".input");
+  let apiKey = "o031ff44025aa0btaa12d97420a0a3c6";
+  let context =
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem and separate each line (each on a new line). Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning. Do not include any title or say anything about the poem before beginning";
+  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemForm = document.querySelector("#poem-generator");
